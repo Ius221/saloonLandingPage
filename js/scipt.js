be02103fe299245ca59fb2020343509ce2c90e76
Ayush <ayush9783@gmail.com>
2025-07-16 15:23:29 +0530

    adding js in hero section

---
+++ js/scipt.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const parentNavBtn = document.querySelector('.hero--nav-btns');
+const allNav = document.querySelectorAll('.hero--nav-btn');
+const heroTextGroup = document.querySelector('.hero--title');
+const heroImage = document.querySelectorAll('.hero--container');
+
+let i = 0;
+
+//////////////////////////////
+// HERO SECTION NAVIGATION //
+////////////////////////////
+
+const changeHero = function (num) {
+    heroImage.forEach(abc => abc.classList.remove('active-hero-container'));
+
+    heroImage[num].classList.add('active-hero-container');
+
+    allNav.forEach(abc => abc.classList.remove('active--nav-btn')
+    )
+    allNav[num].classList.add('active--nav-btn');
+}
+
+parentNavBtn.addEventListener('click', function (e) {
+    if (!e.target.classList.contains('hero--nav-btn')) return;
+    const currImg = Number.parseInt(e.target.getAttribute('data-tag')) - 1;
+    changeHero(currImg);
+})
+
+setInterval(() => {
+    changeHero(i);
+    i += 1;
+    if (i === 3) i = 0;
+}, 5000);
